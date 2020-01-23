788e75c00308dab755fda49e5b2167832cf69c92
Sankara Ekane <sankaraekane@Sankaras-MacBook-Pro.local>
2020-01-22 20:29:31 -0500

    Basic Web server express creation

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import http from "http";
+import express from "express";
+
+const router = express();
+const { PORT = 3000 } = process.env;
+const server = http.createServer(router);
+
+server.listen(PORT, () =>
+  console.log(`server is running http://localHost:${PORT}`)
+);
